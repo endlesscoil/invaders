d993bf674cd074a24a619dd070733d0176729a41
Scott <scott@digression.org>
2014-06-25 07:28:03 -0700

    Moved things around.  Added movement for invaders.

--- invaders/game.py
+++ invaders/game.py
@@ -3,89 +3,9 @@ import random
 
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty, StringProperty
-from kivy.vector import Vector
 from kivy.logger import Logger
 
-
-class Invader(Widget):
-    image = StringProperty('images/invader.jpg')
-
-    MOVE_TIME = 3
-
-    def __init__(self, **kwargs):
-        super(Invader, self).__init__(**kwargs)
-
-        self.last_update = None
-        self.elapsed = 0
-        self.collision_detected = False
-
-    def update(self, dt):
-        self.elapsed += dt
-        if self.elapsed > self.MOVE_TIME or self.last_update is None:
-            self.center_y -= 10
-            self.last_update, self.elapsed = self.elapsed, 0
-
-        return True
-
-
-class Ship(Widget):
-    image = StringProperty('images/ship.jpg')
-    move_direction = NumericProperty(0)
-
-    def __init__(self, **kwargs):
-        super(Ship, self).__init__(**kwargs)
-
-        self.collision_detected = False
-
-    def update(self, dt):
-        if self.move_direction != 0:
-            self.center_x += self.move_direction * 5
-
-            if self.x <= 0:
-                self.x = 0
-            elif self.x + self.width >= self.parent.width:
-                self.x = self.parent.width - self.width
-
-        return True
-
-    def fire(self, velocity=(0, 5)):
-        bullet = Bullet()
-
-        bullet.center_x = self.center_x
-        bullet.center_y = self.y + self.height + 5
-        bullet.velocity = velocity
-
-        return bullet
-
-
-class Bullet(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def __init__(self, **kwargs):
-        super(Bullet, self).__init__(**kwargs)
-
-        self.collision_detected = False
-
-    def update(self, dt):
-        self.pos = Vector(*self.velocity) + self.pos
-
-        # Check for collisions
-        for e in self.parent._entities:
-            if e is not self and e.collide_widget(self):
-                e.collision_detected = True
-
-                return False
-
-        # Check if we've gone off-screen
-        if self.center_y > self.parent.height:
-            return False
-
-        # Still alive.
-        return True
-
+from .entities import Invader
 
 class InvadersGame(Widget):
     def __init__(self):
@@ -116,7 +36,7 @@ class InvadersGame(Widget):
             bullet = self.player_ship.fire()
             self._add_entity(bullet)
 
-        elif keycode[1] in ('left', 'right', 'up', 'down'):
+        elif keycode[1] in ('left', 'right'):
             if keycode[1] == 'left':
                 self.player_ship.move_direction = -1
             elif keycode[1] == 'right':
@@ -131,7 +51,7 @@ class InvadersGame(Widget):
         return True
 
     def _on_key_up(self, keyboard, keycode):
-        if keycode[1] in ('left', 'right', 'up', 'down'):
+        if keycode[1] in ('left', 'right'):
             self.player_ship.move_direction = 0
 
     def _keyboard_closed(self):
